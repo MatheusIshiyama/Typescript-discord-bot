e73e3b1bd2169cef9b8632cb9ef032599fecfeea
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-05-05 13:11:44 -0300

    feat: express server to keep bot on

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import consola from "consola";
 import dotenv from "dotenv";
-import { Bot } from "./client/Client";
 dotenv.config();
 
+import { Bot } from "./client/Client";
+import app from "./server";
+
 const token: string = String(process.env.DISCORD);
+const port: string = String(process.env.PORT);
 
 new Bot().start(token);
 
+app.listen(port, () => consola.success(`[SERVER] Running on PORT: ${port}`));
